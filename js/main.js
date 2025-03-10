6689293832a85f0273136fa7c7fb4f711a81c920
monta <yanxf@mvtech.com.cn>
2025-03-10 15:03:34 +0800

    提交项目基础文件

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ import { initDrawingTool } from './initDrawingTool.js';
 
 const link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
+// link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
+link.href = './css/all.min.css'
 document.head.appendChild(link);
 
 document.addEventListener('DOMContentLoaded', () => {
